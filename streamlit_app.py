1a85bfca2cc6838a98cd966de44268095e007b13
Mateus Benvenuti <mateusjbenvenuti@gmail.com>
2025-08-21 20:47:21 -0300

    Testes

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ def extrair_dados_anuncio(url, aliquota=0, comissao_webvend=0):
 }
     try:
         response = requests.get(url, headers=headers)
-        st.code(response.text, language="html")
     except Exception as e:
         return None, f"Erro ao acessar o link: {e}"
 
